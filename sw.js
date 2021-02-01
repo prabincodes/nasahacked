0a3723ed3ef5142bb4919231be53f0d5e3c974ea
Prabin Subedi <42871240+prabincodes@users.noreply.github.com>
2021-02-02 02:24:07 +0545

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,11 +3,10 @@ const assets = [
   '/',
   '/index.html',
   '/js/app.js',
-  '/js/ui.js',
   '/js/materialize.min.js',
   '/css/styles.css',
   '/css/materialize.min.css',
-  '/img/dish.png',
+  
   'https://fonts.googleapis.com/icon?family=Material+Icons',
   'https://fonts.gstatic.com/s/materialicons/v47/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2'
 ];
