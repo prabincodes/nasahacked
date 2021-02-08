ba6afafbba370a0c1836df54034c8dc26e45b3c9
Prabin Subedi <42871240+prabincodes@users.noreply.github.com>
2021-02-08 19:19:03 +0545

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,7 +6,6 @@ const assets = [
   '/js/materialize.min.js',
   '/css/styles.css',
   '/css/materialize.min.css',
-  
   'https://fonts.googleapis.com/icon?family=Material+Icons',
   'https://fonts.gstatic.com/s/materialicons/v47/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2'
 ];
