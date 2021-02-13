96c4c47ef6f34a045d0c5477284136f4280dbfcb
Prabin Subedi <42871240+prabincodes@users.noreply.github.com>
2021-02-13 22:19:29 +0545

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 if('serviceWorker' in navigator){
-  navigator.serviceWorker.register('/sw.js')
+  navigator.serviceWorker.register('./sw.js')
     .then(reg => console.log('kaam chalau'))
     .catch(err => console.log('bey kaame', err));
 }
