4911d5be4f1b48ac656c0bc9d7fecef725d89e8a
Prabin Subedi <42871240+prabincodes@users.noreply.github.com>
2021-02-13 19:45:45 +0545

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,14 @@
 const staticCacheName = 'site-static-v1';
 const assets = [
-  "/",
-  "/img/favicon.png",
-  '/index.html',
-  '/js/',
-  '/css/'
+"/",
+"/icon-192x192.png",
+"/icon-256x256.png",
+"/icon-384x384.png",
+"/icon-512x512.png",
+"/img/favicon.png",
+'/index.html',
+'/js/',
+'/css/'
   ];
 // install event
 self.addEventListener('install', evt => {
