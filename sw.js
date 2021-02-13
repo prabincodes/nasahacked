b80e83998d3b96600f8177510a0b5ec90bea8416
Prabin Subedi <42871240+prabincodes@users.noreply.github.com>
2021-02-13 19:22:00 +0545

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,35 +1,42 @@
-const cache = 'site-static';
+const staticCacheName = 'site-static-v1';
 const assets = [
   '/',
   '/index.html',
   '/js/app.js',
-  '/js/materialize.min.js',
-  '/sw.js',
-  '/manifest.json',
   '/css/styles.css',
-  '/css/materialize.min.css',
-  ];
+  '/img/images.jpg',
+  'https://fonts.googleapis.com/css2?family=ifanyfont',
+];
 // install event
 self.addEventListener('install', evt => {
-  
   evt.waitUntil(
-    caches.open(cache).then((cache) => {
-  
+    caches.open(staticCacheName).then((cache) => {
+      console.log('caching shell assets');
       cache.addAll(assets);
     })
   );
 });
-
-
+// activate event
 self.addEventListener('activate', evt => {
-  
+  evt.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys
+        .filter(key => key !== staticCacheName)
+        .map(key => caches.delete(key))
+      );
+    })
+  );
 });
-
-
+// fetch event
 self.addEventListener('fetch', evt => {
   evt.respondWith(
     caches.match(evt.request).then(cacheRes => {
-      return cacheRes || fetch(evt.request);
+      return cacheRes || fetch(evt.request).then(fetchRes => {
+        return caches.open(dynamicCacheName).then(cache => {
+          cache.put(evt.request.url, fetchRes.clone());
+          return fetchRes;
+        })
+      });
     })
   );
 });
