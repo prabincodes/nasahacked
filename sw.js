d6f4ca95bd9a7c28f9272576dda459322380c702
Prabin Subedi <42871240+prabincodes@users.noreply.github.com>
2021-02-18 18:30:33 +0545

    Update sw.js

--- sw.js
+++ sw.js
@@ -35,7 +35,7 @@ self.addEventListener("fetch", evt => {
   evt.respondWith(
     caches.match(evt.request).then(cacheRes => {
       return cacheRes || fetch(evt.request).then(fetchRes => {
-        return caches.open(dynamicCacheName).then(cache => {
+        return caches.open(staticCacheName).then(cache => {
           cache.put(evt.request.url, fetchRes.clone());
           return fetchRes;
         })
