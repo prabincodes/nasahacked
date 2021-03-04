454cf52c19af30ac1109d9a9bcb0cac57817f23d
Prabin Subedi <42871240+prabincodes@users.noreply.github.com>
2021-03-05 01:55:31 +0545

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
 const staticCacheName = "daily-rashifal-by-prabin";
+const dynamicCacheName = "newCache";
 const assets = [
 "/",
 "/icon-192x192.png",
@@ -20,22 +21,26 @@ self.addEventListener("install", evt => {
   );
 });
 // activate event
-self.addEventListener("activate", evt => {
+self.addEventListener('activate', evt => {
+  //console.log('service worker activated');
   evt.waitUntil(
     caches.keys().then(keys => {
+      //console.log(keys);
       return Promise.all(keys
-        .filter(key => key !== staticCacheName)
+        .filter(key => key !== staticCacheName && key !== dynamicCacheName)
         .map(key => caches.delete(key))
       );
     })
   );
 });
+
 // fetch event
-self.addEventListener("fetch", evt => {
+self.addEventListener('fetch', evt => {
+  //console.log('fetch event', evt);
   evt.respondWith(
     caches.match(evt.request).then(cacheRes => {
       return cacheRes || fetch(evt.request).then(fetchRes => {
-        return caches.open(staticCacheName).then(cache => {
+        return caches.open(dynamicCacheName).then(cache => {
           cache.put(evt.request.url, fetchRes.clone());
           return fetchRes;
         })
